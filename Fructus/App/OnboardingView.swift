c25876fe0369bd756a3505e404b0695cd12ea39b
Cláudio Cezar <claudiocezar16@gmail.com>
2020-11-26 19:26:30 -0300

    feat: Create OnboardingView

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,32 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Cláudio Cezar on 26/11/20.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
