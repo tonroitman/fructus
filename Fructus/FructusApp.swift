e3c97f7ca316d7ccb194d71161dd2ad31d83919e
Cláudio Cezar <antoniosantos@cwi.com.br>
2020-11-26 15:17:37 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Cláudio Cezar on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
